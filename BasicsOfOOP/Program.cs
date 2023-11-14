be129277be0bc2c4da6c1fb369058b674e0fbcc3
vyagi <marcin.jagiela@hotmail.com>
2023-11-14 16:33:42 +0100

    Some members added

---
+++ BasicsOfOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicsOfOOP
+{
+    public class Student
+    {
+        private string _name;
+        private string _lastName;
+        private int _age;
+        public Student(string name, string lastName, int age)
+        {
+            _name = name;
+            _lastName = lastName;
+            _age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student("James", "Bond" ,20);
+            Console.WriteLine($"Student {s.name} {s.lastName} is {s.age}");
+        }
+    }
+}
