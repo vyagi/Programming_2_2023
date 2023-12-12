35a0d88843d0a74859080b1b8302a3c86954881e
vyagi <marcin.jagiela@hotmail.com>
2023-12-12 17:12:22 +0100

    Finished

---
+++ InvoiceApplication/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace InvoiceApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.categoryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(14, 16);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(34, 15);
+            this.pathLabel.TabIndex = 0;
+            this.pathLabel.Text = "Path:";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(58, 13);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(730, 23);
+            this.pathTextBox.TabIndex = 1;
+            this.pathTextBox.Text = "C:\\Users\\mjagiela\\Desktop\\Invoices.txt";
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(14, 53);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 2;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(14, 94);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(774, 344);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // categoryButton
+            // 
+            this.categoryButton.Location = new System.Drawing.Point(96, 53);
+            this.categoryButton.Name = "categoryButton";
+            this.categoryButton.Size = new System.Drawing.Size(75, 23);
+            this.categoryButton.TabIndex = 4;
+            this.categoryButton.Text = "Category";
+            this.categoryButton.UseVisualStyleBackColor = true;
+            this.categoryButton.Click += new System.EventHandler(this.categoryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.categoryButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Name = "Form1";
+            this.Text = "Invoice Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button categoryButton;
+    }
+}
+
---
+++ InvoiceApplication/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InvoiceApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+            var wholeText = File.ReadAllText(path);
+
+            wholeText = wholeText.Replace(";", "\t");
+
+            resultTextBox.Text = wholeText;
+        }
+
+        private void categoryButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+            var lines = File.ReadAllLines(path);
+
+            var entries = new Dictionary<string, decimal>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i];
+
+                var split = line.Split(";");
+
+                var category = split[2];
+                var price = decimal.Parse(split[1]);
+
+                if (entries.ContainsKey(category))
+                {
+                    entries[category] += price;
+                }
+                else
+                {
+                    entries[category] = price;
+                }
+            }
+
+            resultTextBox.Clear();
+            resultTextBox.Text += "Category\tAmount\r\n";
+
+            foreach (var item in entries)
+            {
+                resultTextBox.Text += $"{item.Key}\t{item.Value}{Environment.NewLine}";
+            }
+
+        }
+    }
+}
